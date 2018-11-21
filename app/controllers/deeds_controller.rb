cc55a28d5284a8d9cb6196940753e10fd3e700b2
Dan Hawkins <danhawkins4@gmail.com>
2018-11-20 18:11:37 -0800

    added deed actions new create show

--- app/controllers/deeds_controller.rb
+++ app/controllers/deeds_controller.rb
@@ -1,2 +1,38 @@
 class DeedsController < ApplicationController
+
+    def index
+    end
+
+    def new
+        @deed = Deed.new
+
+    end
+
+    def create
+        @deed = Deed.new deed_params
+    
+        if @deed.save
+            flash[:success] = "Deed made!"
+            redirect_to deed_path(@deed)
+        else
+            if @deed.errors
+                flash.now[:danger] = @deed.errors.full_messages.join(", ")
+            end
+            render :new
+        end
+
+
+    end 
+
+
+    def show
+        
+    end 
+
+
+    private 
+
+    def deed_params
+        params.require(:deed).permit(:deed, :title, :body, :money_required, :image_url, :location)
+    end
 end
