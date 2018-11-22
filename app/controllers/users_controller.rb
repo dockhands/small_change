5c396b4a25fc0f7ad147c0b631a603c18b24367e
Dan Hawkins <danhawkins4@gmail.com>
2018-11-22 13:06:53 -0800

    add user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,25 @@
 class UsersController < ApplicationController
 
+  before_action :find_user, only: [:edit, :update, :destroy]
+
   def new
       @user = User.new
   end 
 
   def show 
   end 
+
+  def edit
+  end 
+
+
+  def update
+    if @user.update user_params
+    redirect_to user_path(@user.id)
+    else
+    render :edit
+    end
+end
 
   def create
     @user = User.new user_params
