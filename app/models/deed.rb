70561448561a70b018a66e89820557c421b218fb
Dan Hawkins <danhawkins4@gmail.com>
2018-11-22 15:27:18 -0800

    add validations for users and deeds

--- app/models/deed.rb
+++ app/models/deed.rb
@@ -1,4 +1,15 @@
 class Deed < ApplicationRecord
+    validates :title, presence: true, 
+    uniqueness: true,
+    length: { maximum: 60}
+    validates :body, presence: true,
+    length: { minimum: 20, maximum: 500 }
+    validates :money_required, presence: true
+    validates :location, presence: true
+
+
+
+
 
     belongs_to :user 
     has_one_attached :image
