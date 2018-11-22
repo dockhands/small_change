b5c653e55b1c010b534aa6e1a93b01d5aaa3108a
Dan Hawkins <danhawkins4@gmail.com>
2018-11-21 19:37:39 -0800

    add user authorization deed CRUD

---
+++ app/models/ability.rb
@@ -0,0 +1,20 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    # Define abilities for the passed in user here. For example:
+    
+      user ||= User.new # guest user (not logged in)
+      # if user.admin?
+      #   can :manage, :all
+      # else
+      #   can :read, :all
+      # end
+
+      alias_action(:create, :read, :update, :delete, to: :crud)
+
+      can :crud, Deed do |deed|
+        deed.user == user
+      end
+  end
+end
