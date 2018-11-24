3b54c34716a1c25cf247875fc987e728ad490f25
Dan Hawkins <danhawkins4@gmail.com>
2018-11-24 11:40:20 -0800

    wallet increase and decrease on funding and defunding

--- app/controllers/funds_controller.rb
+++ app/controllers/funds_controller.rb
@@ -7,15 +7,16 @@ class FundsController < ApplicationController
 
     def create
       fund          = Fund.new
-      deed          = Deed.find params[:deed_id]
+      deed          = Deed.find_by slug: params[:deed_id]
+      puts params
       fund.deed     = deed
       fund.user     = current_user
+      
       if fund.save
-        redirect_to deeds_path, notice: "Thanks for funding!!"
 
         puts "-----------------------------------------"
         if deed.funds.count === deed.money_required 
-          puts "reached funding"
+        puts "reached funding"
         end 
 
         puts " ============ Deed Creator: "
@@ -25,8 +26,11 @@ class FundsController < ApplicationController
         puts " ============== Deed Funder: "
         puts fund.user.username
         puts fund.user.wallet
-       # fund.user.wallet -1
+        fund.user.wallet -= 1
+        current_user.save
         puts fund.user.wallet 
+        
+        redirect_to deeds_path, notice: "Thanks for funding!!"
 
       else
         redirect_to deed_path(deed), alert: "Can't fund! Already funded?"
@@ -35,8 +39,10 @@ class FundsController < ApplicationController
 
     def destroy
       
-        deed          = Deed.find params[:deed_id]
+        deed     = Deed.find params[:deed_id]
         fund     = current_user.funds.find params[:id]
+        fund.user.wallet += 1
+        current_user.save
         fund.destroy
         redirect_to deeds_path, notice: "Removed funding"
       end
