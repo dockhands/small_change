cf9c19070e7be63057e6d25ecebd658c2e9f474b
Dan Hawkins <danhawkins4@gmail.com>
2018-11-28 15:27:51 -0800

    change app_keys to api_keys.rb

---
+++ config/initializers/api_keys.rb
@@ -0,0 +1,5 @@
+ENV["email_user_name"] = "benjargon@gmail.com"
+ENV["email_password"] = "Pepper12!"
+
+ENV["GITHUB_ID"] = "141336db0cebaea7f7d0"
+ENV["GITHUB_SECRET"] = "02009ff59ee8e0c227b37ca16dc17681c0cab4dd"
